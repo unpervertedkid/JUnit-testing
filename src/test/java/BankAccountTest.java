ecd4b948391b30b42b925a48ae994ca6ba3679da
Brian Silah <unpervertedkid@gmail.com>
2023-03-03 17:09:35 +0300

    Use of assertThrows to ensure methods throw the correct exceptions and use of assertTimeout to ensure methods take some required time to complete

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -2,18 +2,21 @@ import annotations.BankAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 @DisplayName("Bank account tests")
 class BankAccountTest {
 
     @Test
     @DisplayName("Should deposit into an account correctly")
     void depositTest() {
-        BankAccount account = new BankAccount(0,200);
+        BankAccount account = new BankAccount(0, 200);
 
         account.deposit(300);
 
-        assertEquals(500,account.getBalance());
+        assertEquals(500, account.getBalance());
 
         assertThrows(RuntimeException.class, () -> account.deposit(-2));
     }
@@ -21,7 +24,7 @@ class BankAccountTest {
     @Test
     @DisplayName("Should withdraw from an account correctly")
     void withdrawTest() {
-        BankAccount account = new BankAccount(0,500);
+        BankAccount account = new BankAccount(0, 500);
 
         account.withdraw(300);
 
@@ -32,22 +35,35 @@ class BankAccountTest {
 
     @Test
     @DisplayName("Withdrawal for accounts with minimum balance of below zero should allow withdrawals past zero")
-    void withdrawalPastZeroTest(){
+    void withdrawalPastZeroTest() {
         BankAccount account = new BankAccount(-3000, 500);
 
         account.withdraw(2000);
 
         assertNotEquals(0, account.getBalance());
-        assertEquals(-1500,account.getBalance());
+        assertEquals(-1500, account.getBalance());
     }
 
     @Test
     @DisplayName("An account should default to active state after creation")
-    void accountActiveAfterCreation(){
-        BankAccount account = new BankAccount(7888,899);
+    void accountActiveAfterCreation() {
+        BankAccount account = new BankAccount(7888, 899);
 
         assertTrue(account.isActive());
     }
 
+    @Test
+    @DisplayName("Withdrawing below the minimum balance should throw a Runtime exception")
+    void withdrawingBelowMinimumTest() {
+        BankAccount account = new BankAccount(-1000, 500);
 
+        assertThrows(RuntimeException.class, () -> account.withdraw(3000));
+    }
+
+    @Test
+    @DisplayName("Depositing should take less than 1 nanoseconds")
+    void depositTimeoutTest(){
+        BankAccount account = new BankAccount(0, 0);
+        assertTimeout(Duration.ofNanos(1),() -> account.deposit(500));
+    }
 }
