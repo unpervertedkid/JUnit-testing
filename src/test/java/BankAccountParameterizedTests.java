119f7f4a4d024e9e9e0ac8b88d1754279015861e
Brian Silah <unpervertedkid@gmail.com>
2023-03-06 09:38:34 +0300

    Using @EnumSource annotation to pass enum values to a parametrized test

--- src/test/java/BankAccountParameterizedTests.java
+++ src/test/java/BankAccountParameterizedTests.java
@@ -1,7 +1,9 @@
 import annotations.BankAccount;
+import annotations.State;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +17,14 @@ public class BankAccountParameterizedTests {
 
         assertEquals(amount, account.getBalance());
 
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = State.class, names = {"ACTIVE", "INACTIVE"})
+    @DisplayName("Should set the state of an account correctly")
+    void setStateTest(State state, BankAccount account) {
+        account.setState(state);
+
+        assertEquals(state, account.getState());
     }
 }
