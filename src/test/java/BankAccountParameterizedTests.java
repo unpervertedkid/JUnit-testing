1fc381ce69624f525233701aab3097c6bb208746
Brian Silah <unpervertedkid@gmail.com>
2023-03-06 09:44:32 +0300

    Used @CsvSource annotation to use data from a csv file to run tests

--- src/test/java/BankAccountParameterizedTests.java
+++ src/test/java/BankAccountParameterizedTests.java
@@ -3,6 +3,7 @@ import annotations.State;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -26,5 +27,21 @@ public class BankAccountParameterizedTests {
         account.setState(state);
 
         assertEquals(state, account.getState());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "John, 100",
+            "Jane, 200",
+            "Jack, 300",
+            "Jill, 400",
+            "Jenny, 500"
+    })
+    void testDepositAndName(String name, int amount, BankAccount account){
+        account.deposit(amount);
+        account.setHolderName(name);
+
+        assertEquals(amount, account.getBalance());
+        assertEquals(name, account.getHolderName());
     }
 }
